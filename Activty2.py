dc9d934ff4486c451cd9794c9544664486151b16
SwiftieReads12 <sunitarajkanwar@email.com>
2025-10-03 18:36:42 +0100

    Activity3.py

---
+++ Activty2.py
@@ -0,0 +1,4 @@
+name=input (" enter your name ")
+age=12
+job= "student"
+print (name,"hello","welcome to coding")
---
+++ Lesson 1/DataType.py
@@ -0,0 +1,6 @@
+a= 4
+print ("Data type of a", type(a))
+b= 4.6
+print ("Data type of b", type(b))
+c="aura"
+print ("Data type of c", type(c)) 
