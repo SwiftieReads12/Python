e3041f18d38d4219e86e3434b649a8f90b886014
SwiftieReads12 <sunitarajkanwar@email.com>
2025-10-03 18:52:22 +0100

    Activity4.py

---
+++ Lesson 1/TypeCasting.py
@@ -0,0 +1,13 @@
+name= "Aura"
+print("My name is",name)
+print ("Data type of name is",type (name))
+age=13
+
+print("Age", age)
+print("Data type is", type(age))
+
+age= str("age")
+
+print("Data type of age",type(age))
+
+
